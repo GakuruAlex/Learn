5b0e9a5db5d533aa03d176b64be6ee8b50f127d8
alexgicimo <gakurualexgicimo@gmail.com>
2024-05-02 11:38:17 +0000

    Flask app

---
+++ webapp/app.py
@@ -0,0 +1,34 @@
+from flask import Flask,session,render_template
+import os
+from swimclub import process_swim_data,FOLDER
+app = Flask(__name__)
+app.secret_key ="You'll never guess"
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+def populate_data():
+    if "swimmers" not in session:
+         session["swimmers"] = {}
+         filenames = os.listdir(FOLDER)
+         filenames.remove(".DS_Store")
+
+         for file in filenames:
+          name =process_swim_data(file)["name"]
+          if name not in session["swimmers"]:
+            session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
+
+@app.get("/swimmers")
+def display_swimmers()->list:
+    populate_data()
+    return str(sorted(session["swimmers"]))
+    
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
+
+if __name__ == "__main__":
+    app.run()
