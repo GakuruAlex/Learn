9178b8da5166627ebbd76bb9b43ca6336b7e9d11
alexgicimo <gakurualexgicimo@gmail.com>
2024-05-11 20:50:05 +0000

    Use event lookup to match record from the reords.json

--- webapp/barchart.py
+++ webapp/barchart.py
@@ -1,7 +1,26 @@
 from swimclub import process_swim_data
 from hfpy_utils import convert2range
+import json
 import os
 CHARTS ="charts/"
+JSONDATA = "./webapp/world_records.json"
+COURSES = ["LC MEN","LC WOMEN","SC MEN","SC WOMEN"]
+
+
+
+def event_lookup(filename):
+    conversions = {
+    "Free":"freestyle",
+    "Back":"backstroke",
+    "Breast":"breaststroke",
+    "Fly":"butterfly",
+    "IM":"individual medley"
+
+}
+    *_,distance,event = filename.removesuffix(".txt").split("-")
+
+    return f"{distance} {conversions[event]}"
+
 
 def draw_bar_chart(filename,location=CHARTS):
     swimmer = process_swim_data(filename)
@@ -15,8 +34,13 @@ def draw_bar_chart(filename,location=CHARTS):
     stroke = swimmer["stroke"]
     times.reverse()
     converts.reverse()
+    records = []
 
     max_range = max(converts)
+    with open(JSONDATA) as jf:
+        world_records =json.load(jf)
+    for course in COURSES:
+        records.append(world_records[course][event_lookup(filename)])
     for n in converts:
                 ranges.append(convert2range(n,0,max_range,0,350))
                 title = f"{name} (Under {age}) {distance} - {stroke}"
@@ -29,6 +53,13 @@ def draw_bar_chart(filename,location=CHARTS):
                     <h3>{title}</h3>
                     """
                 footer = f"""<p>Average time {average}</p>
+                    <p> <h4>M: {records[0]}
+                            ({records[2]})
+                        </h4>
+                        <h4> W: {records[1]}
+                            ({records[3]})
+                        </h4>
+                    </p>
                     </body>
                     </html>
                     """
